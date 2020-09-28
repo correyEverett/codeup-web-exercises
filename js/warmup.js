0e85441de67cf5e53865963e87e360afd6949101
Correy Everett <correy.everett@gmail.com>
2020-09-28 09:45:12 -0500

    commit correction

--- js/warmup.js
+++ js/warmup.js
@@ -1,11 +1,18 @@
-var name1 = 'Vivian';
-var name2 = 'Sopie';
+'use strict';
+// var name1 = 'Vivian';
+// // var name2 = 'Sopie';
 var name3 = 'Correy';
-var name4 = 'Matt';
-var name5 = 'Salah';
+// // var name4 = 'Matt';
+// // var name5 = 'Salah';
 
-console.log(name1);
-console.log(name2);
-console.log(name3);
-console.log(name4);
-console.log(name5);
+console.log('Hello ' + name1);
+console.log('Hello ' + name2);
+console.log('Hello ' + name3);
+console.log('Hello ' + name4);
+console.log('Hello ' + name5);
+
+function sayHello(name) {
+    console.log('Hello ' + name);
+}
+
+sayHello(name3);
