9e3cd0ce7f1df4f6d7d3277feb8d10494fad41ae
Correy Everett <correy.everett@gmail.com>
2020-09-28 09:41:28 -0500

    created warmup files

---
+++ js/warmup.js
@@ -0,0 +1,11 @@
+var name1 = 'Vivian';
+var name2 = 'Sopie';
+var name3 = 'Correy';
+var name4 = 'Matt';
+var name5 = 'Salah';
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(name4);
+console.log(name5);
