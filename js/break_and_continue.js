e51d95a54a68b8058258bb7fdc3b0908af12b436
Correy Everett <correy.everett@gmail.com>
2020-10-03 15:38:01 -0500

    do-while-loop

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,34 +4,40 @@
 
 
 
-function  isEven(input){
-    return input % 2 === 0;
-}
-
-    var usersNumber = Number(prompt('Please enter an ODD number between 1-50.'));
-
-    for (var i = 1; i < 51; i++){
-        if (isEven(i)) {
-                continue;
-        }
-        if(usersNumber === i){
-            console.log('Yikes! Skipping number:' + i);
-            continue;
-        }
-        console.log('Here is an ODD number:' + i);
-    }
+// function  isEven(input){
+//     return input % 2 === 0;
+// }
+//
+//     var usersNumber = Number(prompt('Please enter an ODD number between 1-50.'));
+//
+//     for (var i = 1; i < 51; i++){
+//         if (isEven(i)) {
+//                 continue;
+//         }
+//         if(usersNumber === i){
+//             console.log('Yikes! Skipping number:' + i);
+//         } else {
+//             console.log('Here is an ODD number:' + i);
+//         }
+//         if(i > 49){
+//             break;
+//         }
+//         if(typeof usersNumber !== 'number'){
+//             var usersNumber = Number(prompt('Please enter an ODD number between 1-50.'));
+//         }
+//     }
 
 
 
 //TODO  3.  Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
 
-for (var i = 1; i < 51; i++){
-    if (isEven(i)) {
-        continue;
-    }
-    if(usersNumber === i){
-        console.log('');
-        continue;
-    }
-    console.log('Here is an ODD number:' + i);
-}
+// for (var i = 1; i < 51; i++){
+//     if (isEven(i)) {
+//         continue;
+//     }
+//     if(usersNumber === i){
+//         console.log('');
+//         continue;
+//     }
+//     console.log('Here is an ODD number:' + i);
+// }
---
+++ js/while.js
@@ -0,0 +1,35 @@
+//TODO  1. Create a file named while.js in the js directory.
+
+//TODO  2. Create a while loop that uses console.log() to create the output shown below.
+
+// var i = 2;
+//
+// while(i <= 65536){
+//     console.log(i);
+//     i *= 2 ;
+// }
+
+/****** DO WHILE LOOP ******/
+
+
+//TODO
+// An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is a way get the random numbers for this exercise.
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+var individualPurchases = Math.floor(Math.random() * 5) + 1;
+
+
+console.log('You have ' + allCones + ' cones left to sell before you can go home for the day.');
+
+do{
+
+    console.log('You just sold ' + individualPurchases + ' Cones!');
+    allCones -= individualPurchases;
+    console.log('You have ' + allCones + ' left' );
+
+    if(allCones === 0){
+        console.log('Yay! I sold them all!');
+    } else if(individualPurchases > allCones) {
+        console.log('I can\'t sell you ' + individualPurchases + ' cones, I only have ' + allCones + ' left!')
+    }
+}while(allCones > 0);
