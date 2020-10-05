bbf389a4a152e3dc1247d1d39bec11e23aa3be01
Correy Everett <correy.everett@gmail.com>
2020-10-05 13:34:40 -0500

    updated work

--- js/while.js
+++ js/while.js
@@ -16,20 +16,28 @@
 // An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is a way get the random numbers for this exercise.
 
 var allCones = Math.floor(Math.random() * 50) + 50;
-var individualPurchases = Math.floor(Math.random() * 5) + 1;
-
-
-console.log('You have ' + allCones + ' cones left to sell before you can go home for the day.');
+console.log('You have ' + allCones + ' cone(s) left to sell before you can go home for the day.');
 
 do{
 
-    console.log('You just sold ' + individualPurchases + ' Cones!');
+    var individualPurchases = Math.floor(Math.random() * 5) + 1;
+    console.log('You just sold ' + individualPurchases + ' Cone(s)!');
     allCones -= individualPurchases;
     console.log('You have ' + allCones + ' left' );
+
 
     if(allCones === 0){
         console.log('Yay! I sold them all!');
     } else if(individualPurchases > allCones) {
-        console.log('I can\'t sell you ' + individualPurchases + ' cones, I only have ' + allCones + ' left!')
+        console.log('I can\'t sell you ' + individualPurchases + ' cones, I only have ' + allCones + ' left!');
+        continue;
     }
+
 }while(allCones > 0);
+
+
+// function caseTest(input){
+//     return input === input.toUpperCase() || input === input.toLowerCase();
+// }
+//
+// console.log(caseTest('rock'));
