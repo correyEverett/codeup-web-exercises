9a21b34501f223755b62549ff7f340fd7a76a424
Correy Everett <correy.everett@gmail.com>
2020-11-13 16:27:12 -0600

    completed traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -16,8 +16,22 @@ $heavenlyBody.on('mouseover', () => {
 });
 
 
+// MAKES THE LAST LIST ITEM IN EACH LIST HAVE A YELLOW BACKGROUND ON BUTTON CLICK
+$('.btn').on('click', () => {
+    $('ul').each(function (index, element) {
+        $(element).children().last().toggleClass('yellow-background');
+    });
+});
 
+//THE LIST TURNS BOLD WHEN THE HEADING IS CLICKED
+$('h3').on('click', (event) => {
+    $(event.currentTarget).next().toggleClass('bolded');
+});
 
+//WHEN ANY LIST ITEM IS CLICKED THE FIRST ITEM IN THAT PARTICULAR LIST TURNS BLUE
+$('li').on('click', (event) => {
+    $(event.currentTarget).parent().children().first().toggleClass('blue-font');
+});
 
 
 
