bf988bfdebe0063845c97789819bd20fc8c565eb
Correy Everett <correy.everett@gmail.com>
2020-11-18 18:41:49 -0600

    Completed buildHTML function

--- js/weather_map.js
+++ js/weather_map.js
@@ -29,26 +29,40 @@
 
 
 
-        function buildHTML (cards) {
-            let cardsHTML = '';
+        function buildHTML (weatherInfo) {
+            console.log(weatherInfo.daily);
+            let daysHTML = '';
 
-            cards.forEach(function(card) {
+            weatherInfo.daily.forEach(function(day) {
+                let dateTime = day.dt * 1000;
+                let myDate = new Date(dateTime);
 
-                cardsHTML += "<div>" + card.dt + "</div>";
-
-                cardsHTML += "<li>" + card.temp +"</li>";
-                cardsHTML += "<li>";
-                cardsHTML += "<span>" + card.description +"</span>";
-                cardsHTML += "<br>";
-                cardsHTML += "<br>";
-                cardsHTML += "<span>" + card.humidity +"</span>";
-                cardsHTML += "</li>";
-                cardsHTML += "<li>" + card.winds +"</li>";
+                daysHTML += `<div class="card" style="width: 18rem;">
+                                 <div class="card-header">
+                                    <span>${myDate}</span>
+                                 </div>
+                                 <ul class="list-group list-group-flush">
+                                    <li class="list-group-item"><span>Temp: ${day.temp.day}</span></li>
+                                    <li class="list-group-item">
+                                        <span>Description: ${day.weather[0].main}</span>
+                                        <br>
+                                        <br>
+                                        <span>Humidity: ${day.humidity}</span>
+                                    </li>
+                                    <li class="list-group-item"><span>Winds: ${day.wind_speed}</span></li>
+                                </ul>
+                            </div>`
             });
-
-            $('.card-header').html(cardsHTML)
-            $('.list-group-item').html(cardsHTML)
+            $('#main').html(daysHTML)
         }
+        updateCards();
 
-        // $('.card-header').click(updateCards);
+        $('.card').each(function (index, element) {
+            if (index > 4) {
+                $(this).hide(element);
+            }
+        });
+
+
     });
+
