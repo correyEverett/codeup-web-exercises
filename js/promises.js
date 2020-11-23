a542ef06ed9d65359784828a476938bcb478e5c8
Correy Everett <correy.everett@gmail.com>
2020-11-23 16:30:49 -0600

    Handle an API Promise #1

--- js/promises.js
+++ js/promises.js
@@ -3,21 +3,32 @@
 
 'use strict';
 
-    const wait = (number) => {
-        return new Promise((resolve, reject) => {
-            const timer = number * 1000;
-            setTimeout(() => {
-                if (typeof number === 'number') {
-                    resolve(number);
-                } else {
-                    reject();
-                }
-            }, timer);
-        });
-    }
+    // const wait = (number) => {
+    //     return new Promise((resolve, reject) => {
+    //         const timer = number * 1000;
+    //         setTimeout(() => {
+    //             if (typeof number === 'number') {
+    //                 resolve(number);
+    //             } else {
+    //                 reject();
+    //             }
+    //         }, timer);
+    //     });
+    // }
+    //
+    // wait(5).then((number) => {
+    //     console.log(`You'll see this after ${number} second(s).` );
+    // }).catch(() => {
+    //     console.log(`You're input is invalid. Pleae try again!`);
+    // });
 
-    wait(5).then((number) => {
-        console.log(`You'll see this after ${number} second(s).` );
-    }).catch(() => {
-        console.log(`You're input is invalid. Pleae try again!`);
-    });
+    fetch('https://api.github.com', {headers: {'Authorization': 'token gitHubKey'}})
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch(() => {
+
+        });
