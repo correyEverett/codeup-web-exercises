ab9430549771ed451b837eb3a1ef009dfc5768f2
Correy Everett <correy.everett@gmail.com>
2020-11-23 14:16:17 -0600

    commit 2

--- js/promises.js
+++ js/promises.js
@@ -4,19 +4,19 @@
 'use strict';
 
     const wait = (number) => {
-        new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
             const timer = number * 1000;
-            setTimeout((timer * 1000) => {
+            setTimeout(() => {
                 if (typeof number === 'number') {
-                    resolve(true);
+                    resolve();
                 } else {
-                    reject(false);
+                    reject();
                 }
             }, timer);
         });
     }
 
-    wait().then(() => {
+    wait(2).then((number) => {
         console.log(`You'll see this after ${number} second(s).` );
     }).catch(() => {
         console.log(`You're input is invalid. Pleae try again!`);
