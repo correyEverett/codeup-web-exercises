f04bf5be9ac37b1d994e741bdf1a30659e90aeeb
Correy Everett <correy.everett@gmail.com>
2021-04-26 18:48:18 -0500

    ex37

--- js/101exercises.js
+++ js/101exercises.js
@@ -601,3 +601,15 @@ assert(quotient(27, 9), 3, "Exercise 36");
 assert(quotient(5, 2), 2, "Exercise 36");
 assert(quotient(10, 3), 3, "Exercise 36");
 addToDone("Exercise 36 is correct.")
+
+
+// Exercise 37
+// Write a function definition named remainder that takes in two numbers and returns the remainder of first argument divided by the second argument.
+function remainder(first, second) {
+    return first % second;
+}
+
+assert(remainder(3, 3), 0, "Exercise 37");
+assert(remainder(5, 2), 1, "Exercise 37");
+assert(remainder(7, 5), 2, "Exercise 37");
+addToDone("Exercise 37 is correct.")
